74cf5d02558531d149afce6353ebc6d543ef125d
Vincent-Murienne <muriennevincent@gmail.com>
2023-10-10 15:12:13 +0100

    Initialisation du Projet

---
+++ PRJ-TemplePerdu/Launcher.h
@@ -0,0 +1,17 @@
+﻿#ifndef LAUNCHER_H
+#define LAUNCHER_H
+
+/*
+*	Enumerations
+* 	D�clarations de structure
+*	Declaration de fonctions
+*
+*/
+struct Character;
+struct Trap;
+
+void Launch();
+void MainMenu();
+void ExitApp();
+
+#endif LAUNCHER_H
---
+++ PRJ-TemplePerdu/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "Launcher.h"
+#define _CRT_SECURE_NO_WARNINGS
+
+
+int main(void)
+{
+
+	Launch();
+	MainMenu();
+	ExitApp();
+
+	return 0;
+}
