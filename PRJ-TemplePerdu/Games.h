e0837c1fa77e2bf2d3de7ef3e51be05bb716b3c9
Vincent MURIENNE <112702451+Vincent-Murienne@users.noreply.github.com>
2023-10-11 10:53:40 +0100

    Delete PRJ-TemplePerdu directory
    refactoring

--- PRJ-TemplePerdu/Games.h
+++
@@ -1,7 +0,0 @@
-#ifndef GAMES_H
-#define GAMES_H
-
-//int Parcours();
-int Games();
-
-#endif GAMES_H
--- PRJ-TemplePerdu/Global.h
+++
@@ -1,7 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-struct Character;
-struct Trap;
-
-#endif GLOBAL_H
